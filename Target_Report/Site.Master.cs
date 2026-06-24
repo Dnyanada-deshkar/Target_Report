50792cb3cb1a44438f28f98cf14b66f58fe8ba25
Abhishek Karande <abhishekkarande17@gmail.com>
2026-06-24 18:03:32 +0530

    fixed redirect issuue (Pages redirect to Login first)

--- Target_Report/Site.Master.cs
+++ Target_Report/Site.Master.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,6 +12,12 @@ namespace Target_Report
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["AdminUsername"] == null)
+            {
+                Response.Redirect("~/Login.aspx");
+                return;
+            }
+
             if (!IsPostBack)
             {
                 HighlightActiveNavLink();
